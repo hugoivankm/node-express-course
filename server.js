9e48d109ec4a05d77b74ab9628a4557fb064ea19
Hugo Ivankovich Martos <hugoivank@gmail.com>
2022-03-17 19:46:47 -0600

    add dynamic GET route.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-
 const MockUserData = [{ name: "Mark" }, { name: "Jill" }];
 
 app.get("/users", function (req, res) {
@@ -9,6 +8,15 @@ app.get("/users", function (req, res) {
     success: true,
     manage: "successfully got users. Nice!",
     users: MockUserData,
+  });
+});
+
+app.get("/users/:id", function (req, res) {
+  console.log(req.params.id);
+  res.json({
+    success: true,
+    manage: "got one user",
+    user: req.params.id,
   });
 });
 
