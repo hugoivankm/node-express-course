8e6ee62921ec5952f286e230714fc1fbe121ffd5
Hugo Ivankovich Martos <hugoivank@gmail.com>
2022-03-21 22:35:54 -0600

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+
 const MockUserData = [{ name: "Mark" }, { name: "Jill" }];
+
+const bodyParser = require("body-parse");
+app.use(bodyParser.json());
 
 app.get("/users", function (req, res) {
   res.json({
@@ -18,6 +22,27 @@ app.get("/users/:id", function (req, res) {
     manage: "got one user",
     user: req.params.id,
   });
+});
+
+app.post('/login', function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "billyTheKid";
+  const mokckPassword = "superSecret";
+
+  if (username === mockUsername && password === mokckPassword) {
+    res.json({
+      success: true,
+      message: "Password and user name match!",
+      token: "encrypted token goes here"
+    })
+  } else {
+    res.json({
+      success: false,
+      message: "Password and user name do not match!"
+    })
+  }
 });
 
 app.listen(8000, function () {
